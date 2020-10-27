98c230c02428cd67002d68df17d493627f1bf1ae
sathyanarayan <sathyanmbpk@gmail.com>
2020-10-27 18:50:11 +0530

    “Assignment”

---
+++ Palindrome-Sathya’s MacBook Air.java
@@ -0,0 +1,19 @@
+//check given no is palindrome or not
+import java.io.*;
+import java.util.*;
+public class Palindrome{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to test palindrome");
+        int num = scanner.nextInt();
+        int temp = num;
+        String y = "";
+        while(temp !=0)
+        {
+            int x = temp%10;
+            y = y+((x);
+
+        }
+    }
+    
+}
